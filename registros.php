fe2c9a2bae7ce0e11291025cc67772af8afabd27
Shechy Torres <shechytorres@gmail.com>
2022-02-18 03:06:20 -0500

    Correcciones hechas en la base de datos

--- registros.php
+++ registros.php
@@ -8,13 +8,13 @@
     <main>
       <div class="contenedor">
         <?php
-          /* $conexion = mysqli_connect("localhost", "root", "", "formulario") or
+          $conexion = mysqli_connect("localhost", "id18464140_root", "Steicytorres.1", "id18464140_formulariomyp") or
           die("Problemas con la conexión");
 
-          mysqli_query($conexion, "insert into cursos(nombre) values ('$_REQUEST[nombre]')") or
+          mysqli_query($conexion, "insert into registros(primernombre,segundonombre,primerapellido,segundoapellido,telefono,email,edad,sexo,mensaje) values ('$_REQUEST[primernombre]','$_REQUEST[segundonombre]','$_REQUEST[primerapellido]','$_REQUEST[segundoapellido]',$_REQUEST[telefono],'$_REQUEST[email]',$_REQUEST[edad],'$_REQUEST[sexo]','$_REQUEST[mensaje]')") or
             die("Problemas en el select" . mysqli_error($conexion));
 
-          mysqli_close($conexion); */
+          mysqli_close($conexion);
 
           echo '<h2>' . "Sus datos fueron cargados exitosamente." . '<h2>';
         ?>
